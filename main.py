ce796b14756ff06803885ed99a89789ea53a54ac
ayyyyyako <120071494+ayyyyyako@users.noreply.github.com>
2022-12-15 15:37:19 +0900

    a

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
 from flask import Flask, request, render_template
+import cv2
+photo = 'upload_img/25390.jpg'
+
+def faceframe(src):
+
+    face_cascade_path = '/Users/k21072kk/.pyenv/versions/3.10.6/lib/python3.10/site-packages/cv2/data/haarcascade_frontalface_default.xml'
+    face_cascade = cv2.CascadeClassifier(face_cascade_path)
+
+    photo = cv2.imread(src)
+    src_gray = cv2.cvtColor(photo, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(src_gray)
+
+    for x, y, w, h in faces:
+        cv2.rectangle(photo, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        
+    cv2.imwrite('result_img/faceframe_img/253902.jpg', photo)
+
+faceframe(photo)
+
+
+
+
+
+
+
 
 app = Flask(__name__)
 
