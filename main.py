710fbe5bbc9259fdc7021cf42acff0d2565ac93f
OkamotoAi <okamotoai0217@icloud.com>
2022-12-16 16:31:11 +0900

    [add] .txtを.gitkeepに変更(画像表示に差し支えがあるため)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_from_directory
 import os
 import glob
 
@@ -18,19 +18,26 @@ def home_post():
     file.save(os.path.join('./upload_img', file.filename))
     return render_template('index.html')
 
-# アップロードした画像を表示
+# アップロードした画像を一覧表示
 @app.route('/upload')
 def upload():
+    # upload_img直下のファイルを全て取得する
     files = glob.glob("upload_img/*")
+    # 画像ファイルへのパス
     urls = []
     for file in files:
         urls.append({
             "filename": os.path.basename(file),
-            "url": "../upload_img/" + os.path.basename(file)
+            "url": "/upload/" + os.path.basename(file)
         })
-    app.logger.info(urls)
     return render_template("show_img.html", target_files=urls)
 
+# 画像を表示
+@app.route('/upload/<path:filename>')
+def upload_show(filename):
+    return send_from_directory('./upload_img', filename)
+
+# グレースケール画像を一覧表示
 @app.route('/gray')
 def gray():
     files = glob.glob("result_img/gray_img/*")
@@ -38,35 +45,49 @@ def gray():
     for file in files:
         urls.append({
             "filename": os.path.basename(file),
-            "url": "/processed/gs/" + os.path.basename(file)
+            "url": "/gray/" + os.path.basename(file)
         })
     return render_template("show_img.html", target_files=urls)
 
+# 画像を表示
+@app.route('/gray/<path:filename>')
+def gray_show(filename):
+    return send_from_directory('./result_img/gray_img', filename)
 
+# 顔枠画像を一覧表示
 @app.route('/faceframe')
 def faceframe():
-    files = glob.glob(".result_img//faceframe_img/*")
+    files = glob.glob("./result_img/faceframe_img/*")
     app.logger.info(files)
     urls = []
     for file in files:
         urls.append({
             "filename": os.path.basename(file),
-            "url": "/processed/gs/" + os.path.basename(file)
+            "url": "/faceframe/" + os.path.basename(file)
         })
     return render_template("show_img.html", target_files=urls)
 
-  
+# 画像を表示
+@app.route('/faceframe/<path:filename>')
+def faceframe_show(filename):
+    return send_from_directory('./result_img/faceframe_img', filename)
+
+# アウトライン画像を一覧表示
 @app.route('/outline')
 def outline():
-    files = glob.glob(".result_img//outline_img/*")
+    files = glob.glob("./result_img/outline_img/*")
     urls = []
     for file in files:
         urls.append({
             "filename": os.path.basename(file),
-            "url": "/processed/gs/" + os.path.basename(file)
+            "url": "/outline/" + os.path.basename(file)
         })
     return render_template("show_img.html", target_files=urls)
 
+# 画像を表示
+@app.route('/outline/<path:filename>')
+def outline_show(filename):
+    return send_from_directory('./result_img/outline_img', filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
